229d0e3eed588353386795f318123df8cfd50585
keyongfeng <keyongfeng@guanghe.tv>
2021-01-04 18:41:00 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import 'element-ui/lib/theme-chalk/index.css';
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+  	<div class="left">
+		<el-input rows="30" @change="change" type="textarea" v-model="raw" />
+	</div>
+  	<div class="right">
+		<el-input rows="30" type="textarea" v-model="result" />
+	</div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import {Input} from 'element-ui';
+import {jsonToGo} from '../util/json-to-go';
+
+Vue.use(Input);
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+		raw: '',
+		result: '',
+    }
+  },
+  methods: {
+	change (){
+		let tmp = jsonToGo(this.raw);
+		this.result = tmp.go == '' ? tmp.error : tmp.go; 
+	},
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.hello{
+		display:flex;
+		flex-flow: row nowrap;
+		justify-content: space-between;
+	}
+	.left, .right{
+		width:49%;
+	}
+</style>
