fd6688e0fcd699927a2eb0c6e55a03631be81814
keyongfeng <keyongfeng@guanghe.tv>
2021-04-01 10:50:57 +0800

    时间戳转换

---
+++ src/components/DateTool.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+  	<div class="left">
+		<el-input @change="change" type="text" v-model="raw" />
+	</div>
+  	<div class="right">
+		<el-input type="text" v-model="result" />
+	</div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import {Input, Message} from 'element-ui';
+
+Vue.use(Input);
+export default {
+  name: 'Date',
+  data () {
+    return {
+		raw: '',
+		result: '',
+    }
+  },
+  methods: {
+	change (){
+		try{
+			let tmp = new Date(this.raw * 1000);
+			this.result = tmp.toLocaleString(); 
+		}catch(e){
+			Message.error(e.toString());
+		}
+	},
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.hello{
+		display:flex;
+		flex-flow: row nowrap;
+		justify-content: space-between;
+	}
+	.left, .right{
+		width:49%;
+	}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import DateTool from '@/components/DateTool'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/date',
+      name: 'Date',
+      component: DateTool
     }
   ]
 })
